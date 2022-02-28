e69d0f6d1d6eb84cb10ab0ecc32c0bc0854adf36
Pablovide <pabloviturrod@gmail.com>
2022-02-28 18:05:35 +0100

    listado de productos

--- ecommerce/frontend/src/views/Home.vue
+++ ecommerce/frontend/src/views/Home.vue
@@ -4,6 +4,22 @@
     <div>
       Listado de productos
     </div>
+    <div class="d-flex flex-wrap justify-content-center mt-2">
+
+    <b-card v-for="product in products" v-bind:key="product.id"
+    :title="product.name"
+    :img-src="product.mainImage"
+    :img-alt="product.name"
+    tag="article"
+    style="max-width:20rem;"
+    class="m-3">
+      <b-card-text>{{product.description.substring(0,100)+ "..."}}</b-card-text>
+      <div class="d-flex justify-content-center" style="gap: 0.5rem;">
+        <b-button variant="primary">View</b-button>
+        <b-button variant="primary">Add</b-button>
+      </div>
+    </b-card>
+    </div>
   </div>
 </template>
 
@@ -13,6 +29,18 @@
 
 export default {
   name: 'Home',
+  created () {
+    fetch('http://localhost:3000/products')
+      .then(response => response.json())
+      .then(json => {
+        this.products = json
+      })
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
   components: {
   }
 }
