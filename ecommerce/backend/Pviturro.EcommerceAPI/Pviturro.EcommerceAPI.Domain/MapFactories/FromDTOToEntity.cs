7351aefcc3108b621a53b329aa4df97a27659b50
Pablovide <pabloviturrod@gmail.com>
2022-03-09 15:03:53 +0100

    Fix en mappers

--- ecommerce/backend/Pviturro.EcommerceAPI/Pviturro.EcommerceAPI.Domain/MapFactories/FromDTOToEntity.cs
+++ ecommerce/backend/Pviturro.EcommerceAPI/Pviturro.EcommerceAPI.Domain/MapFactories/FromDTOToEntity.cs
@@ -12,16 +12,19 @@ namespace Pviturro.EcommerceAPI.Domain.MapFactories
     {
         public static CategoryEntity Map(this Category dto)
         {
-            return new CategoryEntity { Id = dto.Id, Description = dto.Description, Name = dto.Name, Products = dto.Products.Select(_ => _.Map()).ToList() };
+            if (dto == null) return new CategoryEntity();
+            return new CategoryEntity { Id = dto.Id, Description = dto.Description, Name = dto.Name, Products = dto.Products != null ?  dto.Products.Select(_ => _.Map()).ToList() : new List<ProductEntity>() };
         }
 
         public static ProductEntity Map(this Product dto)
         {
+            if (dto == null) return new ProductEntity();
             return new ProductEntity { Id = dto.Id, Category = dto.Category.Map(), Description = dto.Description, Name = dto.Name, price = dto.Price };
         }
 
         public static ShoppingCartEntity Map(this ShoppingCart dto)
         {
+            if (dto == null) return new ShoppingCartEntity();
             return new ShoppingCartEntity { Id = dto.Id, Email = dto.Email, Product = dto.Product.Map(), Quantity = dto.Quantity };
         }
     }
--- ecommerce/backend/Pviturro.EcommerceAPI/Pviturro.EcommerceAPI.Domain/MapFactories/FromEntityToDTO.cs
+++ ecommerce/backend/Pviturro.EcommerceAPI/Pviturro.EcommerceAPI.Domain/MapFactories/FromEntityToDTO.cs
@@ -12,16 +12,19 @@ namespace Pviturro.EcommerceAPI.Domain.MapFactories
     {
         public static Category Map(this CategoryEntity entity)
         {
-            return new Category { Id = entity.Id, Description = entity.Description, Name = entity.Name, Products = entity.Products.Select(_ => _.Map()).ToList()};
+            if (entity == null) return new Category();
+            return new Category { Id = entity.Id, Description = entity.Description, Name = entity.Name, Products = entity.Products!=null ? entity.Products.Select(_ => _.Map()).ToList(): new List<Product>()};
         }
 
         public static Product Map(this ProductEntity entity)
         {
+            if (entity == null) return new Product();
             return new Product { Id = entity.Id, Category = entity.Category.Map(), CategoryId = entity.Category.Id, Description = entity.Description, Name = entity.Name, Price = entity.price };
         }
 
         public static ShoppingCart Map(this ShoppingCartEntity entity)
         {
+            if (entity == null) return new ShoppingCart();
             return new ShoppingCart { Id = entity.Id, Email = entity.Email, Product = entity.Product.Map(), ProductId = entity.Product.Id, Quantity = entity.Quantity };
         }
     }
